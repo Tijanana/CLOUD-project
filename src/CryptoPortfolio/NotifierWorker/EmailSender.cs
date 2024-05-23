2c5cac7204045bf1b9d88a1b8635b28ab62fb3a3
Tijana <dreamit2812@gmail.com>
2024-05-23 18:02:05 +0200

    Added SMTP configuration as fix

--- src/CryptoPortfolio/NotifierWorker/EmailSender.cs
+++ src/CryptoPortfolio/NotifierWorker/EmailSender.cs
@@ -1,6 +1,9 @@
 ﻿using CryptoPortfolioService_Data.Entities;
 using CryptoPortfolioService_Data.Repositories;
+using Newtonsoft.Json;
+using System;
 using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
@@ -9,20 +12,40 @@ namespace NotifierWorker
 {
     static class EmailSender
     {
+        private static readonly string ConfigFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "appsettings.json");
+        private static readonly EmailSettings EmailSettings = LoadEmailSettings();
+
+        private static EmailSettings LoadEmailSettings()
+        {
+            try
+            {
+                string json = File.ReadAllText(ConfigFilePath);
+                return JsonConvert.DeserializeObject<EmailSettings>(json);
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine($"An error occurred when trying to read the configuration file: {ex.Message}");
+                return null;
+            }
+        }
+
         public static async Task<bool> SendNotificationEmail(Alarm alarm)
         {
             try
             {
+                if (EmailSettings == null)
+                {
+                    Trace.WriteLine("An error occurred when trying to send email:\n\tFailed to read email settings from configuration file!");
+                    return false;
+                }
+
                 var _userRepository = new UserRepository();
                 User user = _userRepository.GetUser(alarm.UserId);
                 if (user == null)
                 {
-                    Trace.WriteLine("An error occured when trying to send email:\n\tUser not found!");
+                    Trace.WriteLine("An error occurred when trying to send email:\n\tUser not found!");
                     return false;
                 }
-
-                var emailUser = "feedback.requests.info@gmail.com";
-                var appPassword = "wgmrzjdbnfcbpsdl";
 
                 // Create client
                 var smtp = new SmtpClient
@@ -32,12 +55,12 @@ namespace NotifierWorker
                     EnableSsl = true,
                     DeliveryMethod = SmtpDeliveryMethod.Network,
                     UseDefaultCredentials = false,
-                    Credentials = new NetworkCredential(emailUser, appPassword)
+                    Credentials = new NetworkCredential(EmailSettings.EmailUser, EmailSettings.AppPassword)
                 };
 
                 // Create the email message
                 MailMessage mail = new MailMessage();
-                mail.From = new MailAddress("feedback.requests.info@gmail.com");
+                mail.From = new MailAddress(EmailSettings.EmailUser);
                 mail.To.Add(user.Email);
 
                 // Fill in message
@@ -48,11 +71,17 @@ namespace NotifierWorker
                 await smtp.SendMailAsync(mail);
                 return true;
             }
-            catch
+            catch (Exception ex)
             {
-                Trace.WriteLine("An error occured when trying to send email");
+                Trace.WriteLine($"An error occurred when trying to send email: {ex.Message}");
                 return false;
             }
         }
+    }
+
+    public class EmailSettings
+    {
+        public string EmailUser { get; set; }
+        public string AppPassword { get; set; }
     }
 }
