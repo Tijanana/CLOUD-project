ea14f7c1825d3dcab4276513cf8e0ba6657bcd72
duskakezic <duskance0307@gmail.com>
2024-05-28 14:59:06 +0200

    Added alert fuctionality in the email sender

--- src/CryptoPortfolio/NotificationService_WorkerRole/EmailSender.cs
+++ src/CryptoPortfolio/NotificationService_WorkerRole/EmailSender.cs
@@ -86,10 +86,57 @@ namespace NotificationService_WorkerRole
                 return false;
             }
         }
+
+        public async Task<bool> SendAlertEmail(string endpoint, DateTime timestamp)
+        {
+            try
+            {
+                if (EmailSettings == null)
+                {
+                    Trace.WriteLine("[HEALTH MONITORING SERVICE]: An error occurred when trying to send email:\n\tFailed to read email settings from configuration file!");
+                    return false;
+                }
+
+                // Create client
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(EmailSettings.EmailUser, EmailSettings.AppPassword)
+                };
+
+                // Create the email message
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(EmailSettings.EmailUser);
+                mail.To.Add(EmailSettings.HealthEmailUser);
+
+                // Fill in message
+                mail.IsBodyHtml = true;
+                var body = "<h1>Healt Alert!</h1>";
+                var service = endpoint.Contains("PortfolioService") ? "Portfolio Service" : "Notification Service";
+                body += $"<h3><br>Detected failure of the {service} at {timestamp.Date} : {timestamp.TimeOfDay.ToString("hh\\:mm\\:ss")}!";
+                mail.Body = body;
+                mail.Subject = $"Health Alert";
+
+                // Send the email
+                await smtp.SendMailAsync(mail);
+                Trace.WriteLine($"[HEALTH MONITORING SERVICE]: Alerted failure of {service}");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine($"[HEALTH MONITORING SERVICE]: An error occurred when trying to send email: {ex.Message}");
+                return false;
+            }
+        }
     }
 
     public class EmailSettings
     {
+        public string HealthEmailUser { get; set; }
         public string EmailUser { get; set; }
         public string AppPassword { get; set; }
     }
