e27f54fe968e524c0fb2383bccc5c281b0255b84
duskakezic <duskance0307@gmail.com>
2024-05-29 13:20:28 +0200

    Setup communication between HMS and NS worker roles

--- src/CryptoPortfolio/HealthMonitoringService/WorkerRole.cs
+++ src/CryptoPortfolio/HealthMonitoringService/WorkerRole.cs
@@ -1,11 +1,16 @@
+using CryptoPortfolioService_Data.Entities;
+using Microsoft.Azure;
 using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Diagnostics;
 using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -62,11 +67,60 @@ namespace HealthMonitoringService
 
         private async Task RunAsync(CancellationToken cancellationToken)
         {
-            // TODO: Replace the following with your own logic.
             while (!cancellationToken.IsCancellationRequested)
             {
-                Trace.TraceInformation("Working");
-                await Task.Delay(1000);
+                var endpoints = await GetHealthCheckEndpointsAsync();
+
+                foreach (var endpoint in endpoints)
+                {
+                    bool isHealthy = await CheckHealthAsync(endpoint);
+                    if (isHealthy)
+                    {
+                        Trace.TraceInformation($"Instance at {endpoint} is healthy.");
+                    }
+                    else
+                    {
+                        Trace.TraceWarning($"Instance at {endpoint} is not healthy.");
+                    }
+                }
+
+                Thread.Sleep(new Random().Next(1000, 5000));
+            }
+        }
+
+        private async Task<List<string>> GetHealthCheckEndpointsAsync()
+        {
+            var endpoints = new List<string>();
+
+            var storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("DataConnectionString"));
+            var tableClient = storageAccount.CreateCloudTableClient();
+            var table = tableClient.GetTableReference("HealthCheckEndpoints");
+
+            var query = new TableQuery<EndpointEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "NotificationService"));
+            var results = await table.ExecuteQuerySegmentedAsync(query, null);
+
+            foreach (var entity in results.Results)
+            {
+                endpoints.Add(entity.Url);
+            }
+
+            return endpoints;
+        }
+
+        private async Task<bool> CheckHealthAsync(string url)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetAsync(url);
+                    return response.IsSuccessStatusCode;
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError($"Error checking health of {url}: {ex.Message}");
+                return false;
             }
         }
     }
