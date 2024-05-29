aaa31225608bfc9df250083bbc0bb6d47cd7b8a4
duskakezic <duskance0307@gmail.com>
2024-05-29 13:28:27 +0200

    Added health check entity and repository

---
+++ src/CryptoPortfolio/CryptoPortfolioService_Data/Entities/HealthCheck.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoPortfolioService_Data.Entities
+{
+    public class HealthCheck : TableEntity
+    {
+        public HealthCheck()
+        {
+            PartitionKey = "HealthCheck";
+            RowKey = Guid.NewGuid().ToString();
+        }
+
+        public string Status { get; set; }
+        public string Service { get; set; }
+        public string Instance { get; set; }
+        public new DateTime Timestamp { get; set; }
+    }
+}
